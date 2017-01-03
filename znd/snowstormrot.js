3846bf8665ec00c5fee7a286dda68b9c6e221d4e
etmad <etmad@users.noreply.github.com>
2017-01-04 02:48:49 +0300

    Update snowstormrot.js

--- znd/snowstormrot.js
+++ znd/snowstormrot.js
@@ -280,7 +280,7 @@ var snowStorm = (function(window, document) {
       screenX = (document.documentElement.clientWidth || document.body.clientWidth || document.body.scrollWidth) - (!isIE ? 8 : 0) - storm.flakeRightOffset;
       screenY = storm.flakeBottom || document.documentElement.clientHeight || document.body.clientHeight || document.body.scrollHeight;
     }
-    docHeight = document.body.offsetHeight - 10;
+    docHeight = document.body.offsetHeight;
     screenX2 = parseInt(screenX/2,10);
   };
 
@@ -288,7 +288,7 @@ var snowStorm = (function(window, document) {
     screenX = storm.targetElement.offsetWidth - storm.flakeRightOffset;
     screenY = storm.flakeBottom || storm.targetElement.offsetHeight;
     screenX2 = parseInt(screenX/2,10);
-    docHeight = document.body.offsetHeight - 10;
+    docHeight = document.body.offsetHeight;
   };
 
   this.freeze = function() {
