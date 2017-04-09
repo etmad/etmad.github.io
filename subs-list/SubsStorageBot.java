a50bcce3322688c44807ed1e2efa0ecaa95adb8b
unknown <auto@updater.mad>
2017-04-09 15:49:09 +0300

    autocommit subs-list

--- subs-list/SubsStorageBot.java
+++ subs-list/SubsStorageBot.java
@@ -1,12 +1,9 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.text.*;
 import java.net.*;
+import java.io.*;
+
 
 //import GSInteractor;
 
